5306c8faf45d8ea20327fdb9f381fbdef3f81948
Alaa Mohammed Youssef <alaamuhamed8@gamil.com>
2024-12-30 19:52:05 +0300

    create dynamic error pages

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,12 +1,24 @@
 import { Container } from "react-bootstrap";
-import { Link } from "react-router";
+import { isRouteErrorResponse, Link, useRouteError } from "react-router";
 
 function Error() {
+  const error = useRouteError();
+  let errorStatus: number;
+  let errorStatusText: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorStatus = error.status;
+    errorStatusText = error.statusText;
+  } else {
+    errorStatus = 404;
+    errorStatusText = "Page Not Found";
+  }
   return (
     <Container className=" h-100 p-5">
       <div className=" text-center m-5">
-        <h1 className=" display-1 text-danger fw-bolder">404</h1>
-        <p className=" display-5 fw-bold">Page not found</p>
+        <h1 className=" display-1 text-danger fw-bolder">{errorStatus}</h1>
+        <p className=" display-5 fw-bold">{errorStatusText}</p>
+        <p className=" display-6 ">Looks like yo have reached a non exist page</p>
         <Link to="/" replace={true} className=" btn btn-success">
           Go Back
         </Link>
